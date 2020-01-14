a86feb78d4c19d8809db6d31c25b9201db78c1cf
Spoitler <romain.bonnes@gmail.com>
2020-01-14 18:16:44 +0100

    news side added

--- web/js/menu.js
+++ web/js/menu.js
@@ -3,12 +3,14 @@ document.getElementById("nav-responsive").onclick = (e) =>
     var nav = document.getElementById("navbarNav");
     if(nav.style.display == "block"){
         nav.style.display = "none";
+
         document.getElementsByTagName("body")[0].style.overflow = "";
     }else{
         nav.style.display = "block";
+        nav.style.animation = "menu_display .3s";
         document.getElementsByTagName("body")[0].style.overflow = "hidden";
-    };
-}
+    }
+};
 
 var logo = document.getElementById("logo");
 document.onscroll = () => resize();
@@ -17,12 +19,9 @@ var pos = logo.getBoundingClientRect().y;
 
 function resize() {
     x = window.scrollY;
-
     if(x > pos/2){
         logo.style.animation = "stick .5s normal";
         logo.style.width="100px";
-
-
     }else if( x < pos && logo.style.animation == "0.5s ease 0s 1 normal none running stick" ){
         logo.style.animation = "unstick .5s normal";
         logo.style.width="250px";
@@ -49,6 +48,21 @@ var slider = tns({
     controlsContainer: "#customize-controls",
    // controls: false,
     autoHeight: "50vh",
+    autoplayText: ["",""],
+    autoplayButton: ".test"
+});
+
+var slider = tns({
+    container: '.slider-news',
+    items: 5,
+    slideBy: "page",
+    // autoplay: true,
+    speed: 800,
+    mouseDrag: true,
+    swipeAngle: false,
+    nav: false,
+    controlsContainer: "#customize-controls",
+    // controls: false,
     autoplayText: ["",""],
     autoplayButton: ".test"
 });
