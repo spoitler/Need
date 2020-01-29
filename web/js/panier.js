176d02a8b1493c1254506212ed64d5ac8361fc32
Spoitler <romain.bonnes@gmail.com>
2020-01-29 18:01:06 +0100

    add promo code input

--- web/js/panier.js
+++ web/js/panier.js
@@ -16,3 +16,17 @@ moins.onclick = () => {
     count -= 1;
     quantite.innerText = count;
 };
+
+function promoCode() {
+    let code = document.getElementsByClassName("container-promo-code");
+
+    console.log(code);
+    console.log(code[0].style.display);
+
+    if (code[0].style.display === "none" || code[0].style.display === ""){
+        code[0].style.display = "flex";
+    }else {
+        code[0].style.display = "none";
+    }
+
+}
