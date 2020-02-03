acb98096dc0d5fe9d570f3963b478ed8822819dc
romain <romain.bonnes@gmail.com>
2020-02-03 23:09:43 +0100

    add profile's pages

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -9,11 +9,38 @@ class UserController extends Controller
 {
 
 	/**
-	 * @Route ("/user", name="need_user")
+	 * @Route ("/profile", name="need_user")
 	 */
 
 	public function indexAction()
 	{
 		return $this->render('user.html.twig', array());
+	}
+
+	/**
+	 * @Route ("/profile/info", name="need_info")
+	 */
+
+	public function infoAction()
+	{
+		return $this->render('user_info.html.twig', array());
+	}
+
+	/**
+	 * @Route ("/profile/modification-password", name="need__modif_password")
+	 */
+
+	public function passwordAction()
+	{
+		return $this->render('password_update.html.twig', array());
+	}
+
+	/**
+	 * @Route ("/profile/commandes", name="need__commandes")
+	 */
+
+	public function commandeAction()
+	{
+		return $this->render('commandes.html.twig', array());
 	}
 }
